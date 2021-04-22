e5bd640d4749084eca1e0f5f1fc293c354858266
Nathan <nathan.fox@ufl.edu>
2021-04-22 01:08:37 -0400

    WriteBoard function implemented

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,9 +1,24 @@
 #file = open("testing.txt", "w")
 #file.close()
+import numpy
 
-def WriteBoard():
+def WriteBoard(puzzle):
     file = open("testing.txt", "w")
 
+    tester = ""
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[i])):
+            tester += str(puzzle[i][j])
+            if ((j + 1) % 3 == 0 and j != 8):
+                tester += " | "
+        tester +="\n"
+        if ((i + 1) % 3 == 0 and i != 8):
+            tester += "---------------\n"
+
+    file.write(tester)
+    file.close()
+
+def EmptyBoard():
     puzzle = []
     tempStore = []
 
@@ -15,24 +30,17 @@ def WriteBoard():
         
         puzzle.append(tempStore)
 
-    tester = ""
-    for i in range(9):
-        for j in range(9):
-            tester += str(puzzle[i][j])
-            if ((j + 1) % 3 == 0 and j != 8):
-                tester += " | "
-        tester +="\n"
-        if ((i + 1) % 3 == 0 and i != 8):
-            tester += "---------------\n"
-
-    file.write(tester)
-    file.close()
     return puzzle
 
 def Main():
     #Main function here
-    puzzle = WriteBoard()
+    puzzle = EmptyBoard()
+    puzzle2 = puzzle[2].copy()
+    puzzle2.insert(0, 2)
+    print(puzzle2)
     print(puzzle)
+    puzzle[2] = puzzle2
+    WriteBoard(puzzle)
     return 0
 
 Main()
